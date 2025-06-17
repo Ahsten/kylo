5174d650d55ac1bccbd64039253fb3f8c7920596
Ahsten <ahanson188@gmail.com>
2025-06-16 20:43:08 -0700

    Disable echo in term

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,38 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+const stdin = std.io.getStdIn().reader();
+const stdout = std.io.getStdOut().writer();
+const Termios = std.posix.termios;
+const posix = std.posix;
+
+const Editor = struct {
+    const Self = @This();
+    orig_termios: ?Termios = null,
+    in: std.fs.File,
+
+    fn enableRawMode(self: *Self) !void {
+        var raw_termios: Termios = undefined;
+        if (self.orig_termios) |orig_termios| {
+            raw_termios = orig_termios;
+        } else {
+            raw_termios = try posix.tcgetattr(self.in.handle);
+            self.orig_termios = raw_termios;
+        }
+
+        raw_termios.lflag.ECHO = false;
+
+        try posix.tcsetattr(self.in.handle, std.posix.TCSA.FLUSH, raw_termios);
+    }
+};
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    var editor = Editor{
+        .in = std.io.getStdIn(),
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    _ = try editor.enableRawMode();
+    var char: u8 = undefined;
+    while (true) {
+        char = try stdin.readByte();
+        if (char == 'q') break;
+        try stdout.print("{c}\n", .{char});
+    }
 }
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("kylo_lib");
